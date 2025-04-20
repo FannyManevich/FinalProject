61765845bef06c92cbe93c6394e3f0d04784ac1f
amirso6 <102460780+amirso6@users.noreply.github.com>
2025-04-20 11:16:16 +0300

    added scripts from main

---
+++ Final Project/Assets/Scripts/Channels/BeaconOS.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class BeaconOS : MonoBehaviour
+{
+    public InputChannel inputChannel;
+    public GameStateChannel gameStateChannel;
+}
---
+++ Final Project/Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,94 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject generalPanel;
+    [SerializeField] GameObject selectPlayerPanel;
+
+    public TextMeshProUGUI panelTitleText;
+    public TextMeshProUGUI panelBodyText;
+    
+    [SerializeField] Button settingsButton;
+    [SerializeField] Button creditsButton;
+    [SerializeField] Button helpButton;
+    [SerializeField] Button quitButton;
+    [SerializeField] Button cancelButton;
+
+    public GameObject malePlayerPrefab;
+    public GameObject femalePlayerPrefab;
+
+    public void OnPlayClicked()
+    {
+        Debug.Log("Play Button clicked");
+        selectPlayerPanel.SetActive(true);
+    }
+
+    public void OnMaleSelected()
+    {
+        Debug.Log("Male button clicked");
+        Time.timeScale = 2f;
+        PlayerSelector.selectedPlayer = malePlayerPrefab;
+        SceneManager.LoadScene("Store");
+    }
+
+    public void OnFemaleSelected()
+    {
+        Debug.Log("Female button clicked");
+        Time.timeScale = 2f;
+        PlayerSelector.selectedPlayer = femalePlayerPrefab;
+        SceneManager.LoadScene("Store");
+    }
+
+    public void ActiveGeneralPanel(string type)
+    {
+        cancelButton.gameObject.SetActive(true);
+        generalPanel.SetActive(true);
+
+        Debug.Log(type);
+
+        switch (type)
+        {
+            case "Settings":
+                panelTitleText.text = "Settings";
+                panelBodyText.text = "You can mute music here";
+                break;
+            case "Credits":
+                panelTitleText.text = "Credits";
+                panelBodyText.text = "Mentor: " +
+                    "       Ella Luna Pleasance" +
+                    " Programmers:" +
+                    "       Fany Manevich, Amir Melicson " +
+                    "Animators:" +
+                    "       Tal Kerklies, Yarin Peled";
+                break;
+            case "Help":
+                panelTitleText.text = "Help";
+                panelBodyText.text = "Choose player." +
+                    "Move with arrows or WSDA. " +
+                    "Help the customers choose the plant of there dreams.";
+                break;
+            default:
+                panelTitleText.text = "Panel not working";
+                panelBodyText.text = "ERROR";
+                break;
+        }
+
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    public void ClosePanelButton()
+    {
+        cancelButton.gameObject.SetActive(false);
+        generalPanel.SetActive(false);
+    }
+}
